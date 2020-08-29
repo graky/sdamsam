5dccd9d83da8dc9b984051fa90851a425f45b56e
grakky <akimgrebenkin21@gmail.com>
2020-08-29 23:20:39 +0300

    small mistake

--- exams/models.py
+++ exams/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class Predmet(models.model):
-    class_text = models.CharField(max_length=200)
+class Predmet(models.Model):
+    predmet_text = models.CharField(max_length=200)
     def __str__(self):
-        return self.class_text
+        return self.predmet_text
 
 # Create your models here.
