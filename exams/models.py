7ac73e0c1facec79861d68904248cdeea4773382
grakky <akimgrebenkin21@gmail.com>
2020-08-29 23:10:36 +0300

    class model added

--- exams/models.py
+++ exams/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-class Subject(models.model):
-
+class Predmet(models.model):
+    class_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.class_text
 
 # Create your models here.
